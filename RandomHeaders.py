96ab9e2f3f02fb57f82c122c0ee6bfe43618c474
Ubaldo Cotta <ucotta@me.com>
2018-06-04 17:55:36 +0200

    Reformatted following PEP8. Added a test by executing this script.

--- RandomHeaders.py
+++ RandomHeaders.py
@@ -1,12 +1,16 @@
 import random
 import csv
 
-
 UserAgentCSV = open('UserAgent.csv', 'r')
 UserAgentList = csv.reader(UserAgentCSV)
 UserAgentList = [row for row in UserAgentList]
 UserAgentList = [l[0] for l in UserAgentList]
 random.shuffle(UserAgentList)
 
+
 def LoadHeader():
-	return {'User-Agent': random.choice(UserAgentList)}
+    return {'User-Agent': random.choice(UserAgentList)}
+
+
+if __name__ == "__main__":
+    print(LoadHeader())
